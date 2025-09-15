6127b005e4a52ff13d404165d55076116d435882
Yuanyao Lin <yuanyaolin13@gmail.com>
2025-09-14 21:02:21 -0400

    Attempt to fix Routing pt.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from "react-router";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 import './index.css'
 
