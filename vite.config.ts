9610952c86ebf3ba2d37c0e6f0a246c23160eff5
Yuanyao Lin <yuanyaolin13@gmail.com>
2025-09-16 12:44:03 -0400

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [
       react(),
       tailwindcss()
   ],
-  base: '/',
 })
